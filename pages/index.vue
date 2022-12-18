d08678c1d20949b5fc0a9c296a04a26b2617f1d0
小畑佑馬 <yuumaobata1008@outlook.com>
2022-12-19 00:46:24 +0900

    初期コミット

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="pageDashboard">
+    
+  </div>
+</template>
+
+<script>
+  import API from '@/api';
+  export default {
+    layout: 'dashboard',
+    components: {
+    },
+  };
+</script>
---
+++ pages/terminal.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="pageDashboard">
+      
+    </div>
+  </template>
+  
+  <script>
+    import API from '@/api';
+    export default {
+      layout: 'dashboard',
+      components: {
+      },
+    };
+  </script>
+  
